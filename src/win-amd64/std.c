d1946cabd6d3046bbf961b29f47e3ec3b75b6f28
Rin Bdlt <rin.bdlt@outlook.com>
2021-03-12 17:02:28 +0100

    Versioning update

--- src/win-amd64/std.c
+++ src/win-amd64/std.c
@@ -1,6 +1,6 @@
 /** Windows amd64 `C` files
  * @file std.c - implementation
- * @version 2020/11 0.3.0.0002
+ * @version 2020/11 0.3.0
  * @author Rin Baudelet (madeshiro)
  */
 #include "zsr_std.h"
